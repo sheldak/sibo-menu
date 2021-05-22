c85bb36d2699ba3966cd1a9b30f4029b98d5cb24
marcinwasowicz <marcinkwasowicz@gmail.com>
2021-05-22 14:55:46 +0200

    Graph creation significantly speed up by numpy where function.

--- aco_main.py
+++ aco_main.py
@@ -3,7 +3,7 @@ from genetic_main import get_loader_from_file
 
 import json
 import numpy as np
-import pickle
+
 
 config = json.load(open('config.json'))
 np.random.seed(config['numpy_random_state'])
@@ -19,12 +19,7 @@ def main():
     products_range = config['restrictions']['products_number']
     product_count = np.random.randint(products_range[0], products_range[1])
     per_category_count = [max(1, product_count // len(categories))] * len(categories)
-
-    try:
-        aco_graph = pickle.load(open('.pickled/aco_graph.pkl', 'rb'))
-    except FileNotFoundError:
-        aco_graph = AcoGraph(categories, category_count, loader, config['product_copies'])
-        pickle.dump(aco_graph, open('.pickled/aco_graph.pkl', 'w+b'))
+    aco_graph = AcoGraph(categories, category_count, loader, config['product_copies'])
 
 
 if __name__ == '__main__':
